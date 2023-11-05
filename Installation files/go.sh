2612f56ee5ba4ac81f30b1b2d7e7009f0b976228
Ankush H V <anshhv2003@gmail.com>
2023-11-06 00:04:37 +0530

    updated go installation file

--- Installation files/go.sh
+++ Installation files/go.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shifting to home directory
 start_dir=$(pwd)
 cd
 
@@ -10,12 +11,18 @@ echo "======================"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+echo "======================"
+echo " Downloafing GO installation file "
+echo "======================"
+
 wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
 
 tar -xvf go1.21.3.linux-amd64.tar.gz
 
 if [ -d "/usr/local/go" ]; then
+	echo "======================"
 	echo "Deleting previous installation"
+	echo "======================"
 	sudo rm -rf /usr/local/go
 fi
 
@@ -26,4 +33,10 @@ echo "export GOPATH=\$HOME/go" >> ~/.bashrc
 echo "export PATH=\$PATH:\$GOPATH/bin:\$GOROOT/bin" >> ~/.bashrc
 source ~/.bashrc
 
+echo "======================"
+echo " Installation Complete "
 go version
+echo "======================"
+
+# By
+# ANKUSH H V
